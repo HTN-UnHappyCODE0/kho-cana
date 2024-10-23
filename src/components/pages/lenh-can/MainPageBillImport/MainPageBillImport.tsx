2c32571f60cfc4fb43a330f46c62c62a52d0b415
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-10-23 16:58:33 +0700

    ff

--- src/components/pages/lenh-can/MainPageBillImport/MainPageBillImport.tsx
+++ src/components/pages/lenh-can/MainPageBillImport/MainPageBillImport.tsx
@@ -344,12 +344,20 @@ function MainPageBillImport({}: PropsMainPageBillImport) {
 							// 	),
 							// },
 							{
-								title: 'Từ',
+								title: 'Từ(tàu/xe)',
 								render: (data: IDataBill) => (
 									<>
 										<p style={{marginBottom: 4, fontWeight: 600}}>{data?.fromUu?.name || data?.customerName}</p>
 										{/* <p>({data?.fromUu?.parentUu?.name || '---'})</p> */}
-										<p style={{fontWeight: 400, color: '#3772FF'}}>{data?.batchsUu?.shipUu?.licensePalate || '---'}</p>
+										{/* <p style={{fontWeight: 400, color: '#3772FF'}}>{data?.batchsUu?.shipUu?.licensePalate || '---'}</p> */}
+										{data?.scalesType == TYPE_SCALES.CAN_XUAT && (
+											<p style={{fontWeight: 400, color: '#3772FF'}}>{'---'}</p>
+										)}
+										{!(data?.scalesType == TYPE_SCALES.CAN_XUAT) && (
+											<p style={{fontWeight: 400, color: '#3772FF'}}>
+												{data?.batchsUu?.shipUu?.licensePalate || '---'}
+											</p>
+										)}
 									</>
 								),
 							},
@@ -358,9 +366,17 @@ function MainPageBillImport({}: PropsMainPageBillImport) {
 								render: (data: IDataBill) => (
 									<>
 										<p style={{marginBottom: 4, fontWeight: 600}}>{data?.toUu?.name || '---'}</p>
-										<p style={{fontWeight: 400, color: '#3772FF'}}>
-											{data?.batchsUu?.shipOutUu?.licensePalate || '---'}
-										</p>
+										{data?.scalesType == TYPE_SCALES.CAN_XUAT && (
+											<p style={{fontWeight: 400, color: '#3772FF'}}>
+												{data?.batchsUu?.shipUu?.licensePalate || '---'}
+											</p>
+										)}
+										{!(data?.scalesType == TYPE_SCALES.CAN_XUAT) && (
+											<p style={{fontWeight: 400, color: '#3772FF'}}>
+												{data?.batchsUu?.shipOutUu?.licensePalate || '---'}
+											</p>
+										)}
+
 										{/* <p>({data?.toUu?.parentUu?.name || '---'})</p> */}
 									</>
 								),
