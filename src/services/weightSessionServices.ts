c8741a17132effa826fe9b2bee475bdf8818c64c
HTN-UnHappyCODE0 <2051063590@e.tlu.edu.vn>
2024-11-22 17:30:18 +0700

    ff

--- src/services/weightSessionServices.ts
+++ src/services/weightSessionServices.ts
@@ -33,6 +33,37 @@ const weightSessionServices = {
 			cancelToken: tokenAxios,
 		});
 	},
+	listTotalWeightsession: (
+		data: {
+			pageSize: number;
+			page: number;
+			keyword: string;
+			status: number[];
+			isDescending: CONFIG_DESCENDING;
+			typeFind: CONFIG_TYPE_FIND;
+			isPaging: CONFIG_PAGING;
+			scalesType: number[];
+			billUuid: string;
+			truckUuid: string;
+			storageUuid: string;
+			isBatch: number | null;
+			timeStart: string | null;
+			timeEnd: string | null;
+			specUuid: string | null;
+			codeStart: number | null;
+			codeEnd: number | null;
+			customerUuid?: string | null;
+			productTypeUuid?: string | null;
+			shift?: number | null;
+			shipUuid?: string;
+			scalesStationUuid?: string;
+		},
+		tokenAxios?: any
+	) => {
+		return axiosClient.post(`/WeightSession/get-total-weightsession`, data, {
+			cancelToken: tokenAxios,
+		});
+	},
 	updateSpecWeightSession: (
 		data: {
 			wsUuids: string[];
